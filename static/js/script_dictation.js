8cac8689f401418d1879c144c27e211b3321189c
Tanya Yushyna <tanyayushyna@192.168.1.117>
2026-03-06 17:14:49 +0300

    0144

--- static/js/script_dictation.js
+++ static/js/script_dictation.js
@@ -5,7 +5,7 @@
 window.originalAudioVisual = window.originalAudioVisual || null;
 window.translationPlayButton = window.translationPlayButton || null;
 
-window.__DICTATION_BUILD = '2026-02-26_0143';
+window.__DICTATION_BUILD = '2026-02-26_0144';
 console.warn('[DICTATION BUILD]', window.__DICTATION_BUILD);
 
 function ensureSwStatusBar() {
