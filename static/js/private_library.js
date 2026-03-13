622137d734d67b08502e43d4968e40af25b9fbda
Tanya Yushyna <tanyayushyna@192.168.1.101>
2026-03-13 06:18:56 +0300

    0200

--- static/js/private_library.js
+++ static/js/private_library.js
@@ -1,7 +1,7 @@
 // Скрипт для новой страницы приватной библиотеки
 
 (function () {
-  window.__PRIVATE_LIBRARY_BUILD = '2026-03-11_0198';
+  window.__PRIVATE_LIBRARY_BUILD = '2026-03-11_0200';
   console.warn('[PRIVATE LIBRARY BUILD]', window.__PRIVATE_LIBRARY_BUILD);
 
   // Debug helper: capture clicks globally to understand if modal buttons are actually receiving events.
