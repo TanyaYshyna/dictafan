46d3e96673355b0f7abcd933b11aeb87af2ddfa7
Tanya Yushyna <tanyayushyna@gmail.com>
2026-05-27 11:50:29 +0300

    0650

--- routes/desktop.py
+++ routes/desktop.py
@@ -1,7 +1,6 @@
 import logging
 
 from flask import Blueprint, render_template
-from flask_jwt_extended import jwt_required
 
 
 logger = logging.getLogger(__name__)
@@ -10,6 +9,5 @@ desktop_bp = Blueprint("desktop", __name__)
 
 
 @desktop_bp.route("/desktop")
-@jwt_required()
 def desktop_page():
     return render_template("desktop.html")
